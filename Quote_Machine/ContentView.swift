30f7da0ede3c88b2eb16c16995f21c8ec272bdd8
Michael <lef17004@byui.edu>
2020-10-14 19:12:57 -0600

    Initial Commit

---
+++ Quote_Machine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quote_Machine
+//
+//  Created by Michael Le Fevre on 10/14/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quote_Machine/Quote_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quote_MachineApp.swift
+//  Quote_Machine
+//
+//  Created by Michael Le Fevre on 10/14/20.
+//
+
+import SwiftUI
+
+@main
+struct Quote_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
